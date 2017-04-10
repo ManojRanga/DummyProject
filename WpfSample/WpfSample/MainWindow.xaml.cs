6a6a4491b7663d4b1001140831ba36beb8f385ad
ManojRanga <saimanoj.ranga@ggktech.com>
2017-04-10 14:20:54 +0530

    commitOne

---
+++ WpfSample/WpfSample/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Employee> employees = new List<Employee>();
+            employees.Add(
+            new Employee
+            {
+                SNo = 1,
+                Name = "Manoj",
+                Age = 23
+            });
+            employees.Add(
+            new Employee
+            {
+                SNo = 2,
+                Name = "Mahesh",
+                Age = 23
+            });
+            employees.Add(
+            new Employee
+            {
+                SNo = 3,
+                Name = "Suresh",
+                Age = 23
+            });
+            EmpGrid.ItemsSource= employees;
+        }
+    }
+}
